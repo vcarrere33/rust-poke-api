b19085019b4acdd3172ae3da350f47c1a4e2dcbd
vcarrere33 <vcarrere33@gmail.com>
2023-01-02 09:52:46 +0100

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
+use actix_web::{web, App, HttpServer};
+mod hello;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     println!("Hello, world!");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello::hello)
+            .route("/hey", web::get().to(hello::manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
