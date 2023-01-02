c33ef7c02354c3ce3113a333f30c07797435e77e
vcarrere33 <vcarrere33@gmail.com>
2023-01-02 10:51:18 +0100

    with mongo

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use actix_web::{web, App, HttpServer};
 mod hello;
+struct Pokemon;
+use actix_web::{web, App, HttpServer};
+use mongodb::{bson::doc, options::IndexOptions, Client, IndexModel};
+
+const DB_NAME: &str = "myApp";
+const COLL_NAME: &str = "pokemon";
+
+async fn create_username_index(client: &Client) {
+    let options = IndexOptions::builder().unique(true).build();
+    let model = IndexModel::builder()
+        .keys(doc! { "username": 1 })
+        .options(options)
+        .build();
+    client
+        .database(DB_NAME)
+        .collection::<Pokemon>(COLL_NAME)
+        .create_index(model, None)
+        .await
+        .expect("creating an index should succeed");
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hello, world!");
-    HttpServer::new(|| {
+    let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+
+    let client = Client::with_uri_str(uri).await.expect("failed to connect");
+    create_username_index(&client).await;
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(web::Data::new(client.clone()))
             .service(hello::hello)
             .route("/hey", web::get().to(hello::manual_hello))
     })
